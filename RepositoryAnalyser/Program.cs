7423a3da82f5c5c874801fc85482ec89e03f74b8
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2016-11-16 18:01:29 +0300

    initial version

---
+++ RepositoryAnalyser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Options;
+
+namespace RepositoryAnalyser
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var ops = new OptionSet()
+			{
+				{ "h|?|help", v => ShowHelp() },
+			};
+			List<string> unparsed = ops.Parse(args);
+			if (unparsed.Count == 0)
+			{
+				ShowHelp();
+			}
+			foreach (string url in unparsed)
+			{
+				ProcessUrl(url);
+			}
+		}
+		static void ProcessUrl(string url)
+		{
+			Console.WriteLine(url);
+		}
+		public static void ShowHelp()
+		{
+			Console.WriteLine("Usage: ");
+			Console.WriteLine("\trepository-analyser http://github.com/Arsen.Shnurkov/repository-analyser");
+			Console.WriteLine("\trepository-analyser -h");
+			Environment.Exit(0);
+		}
+	}
+}
---
+++ RepositoryAnalyser/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("RepositoryAnalyser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Arsen.Shnurkov")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyConfiguration("")]
